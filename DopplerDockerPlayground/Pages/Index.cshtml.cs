462af2ac5bf791890414e0fe24077f91edc2ec9a
Andrés Moschini <andresmoschini@gmail.com>
2019-10-31 20:02:44 -0300

    feat: add some server information in index page

--- DopplerDockerPlayground/Pages/Index.cshtml.cs
+++ DopplerDockerPlayground/Pages/Index.cshtml.cs
@@ -10,10 +10,21 @@ namespace DopplerDockerPlayground.Pages
 {
     public class IndexModel : PageModel
     {
+        private static readonly string _serverId = Guid.NewGuid().ToString();
+        public string ServerId => _serverId;
+
+        private static int _serverCounter = 0;
+        public int ServerCounter { get; }
+
+        public string MachineName { get; } = System.Environment.MachineName;
+
+        public string GetHostName { get; } = System.Net.Dns.GetHostName();
+
         private readonly ILogger<IndexModel> _logger;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            ServerCounter = System.Threading.Interlocked.Add(ref _serverCounter, 1);
         }
 
         public void OnGet()
