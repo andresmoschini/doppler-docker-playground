9302650bd3ab7ef6378a8ee7d05d5dad9f506b57
Andrés Moschini <andresmoschini@gmail.com>
2019-11-14 05:55:23 -0300

    feat: show the value of ASPNETCORE_ENVIRONMENT environment variable

--- DopplerDockerPlayground/ServerStatus.cs
+++ DopplerDockerPlayground/ServerStatus.cs
@@ -9,6 +9,7 @@ namespace DopplerDockerPlayground
         public string ServerId { get; } = Guid.NewGuid().ToString();
         public string MachineName { get; } = Environment.MachineName;
         public string GetHostName { get; } = Dns.GetHostName();
+        public string AspNetCoreEnvironment { get; } = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         int _serverCounter = 0;
         public int GetAndIncrementCounter() => Interlocked.Add(ref _serverCounter, 1);
     }
