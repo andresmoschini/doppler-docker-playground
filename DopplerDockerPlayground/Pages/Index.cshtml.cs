941b603eb08981e7609c0f10f30b6ae1822569c0
Andrés Moschini <andresmoschini@gmail.com>
2019-11-14 06:32:02 -0300

    feat: show all configuration settings in index

--- DopplerDockerPlayground/Pages/Index.cshtml.cs
+++ DopplerDockerPlayground/Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
 
 namespace DopplerDockerPlayground.Pages
 {
@@ -15,15 +16,19 @@ namespace DopplerDockerPlayground.Pages
 
         public PlaygroundSettings PlaygroundSettings { get; }
 
+        public IEnumerable<KeyValuePair<string, string>> ConfigurationEntries { get; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger,
             ServerStatus serverStatus,
-            IOptions<PlaygroundSettings> playgroundSettingsAccessor)
+            IOptions<PlaygroundSettings> playgroundSettingsAccessor,
+            IConfiguration configuration)
         {
             _logger = logger;
             ServerStatus = serverStatus;
             PlaygroundSettings = playgroundSettingsAccessor.Value;
+            ConfigurationEntries = configuration.AsEnumerable();
         }
 
         public void OnGet()
